3a8d92e13148f485260199f20b5d2272fb2d8469
blair <blair@tentwentyfour.ca>
2014-04-04 14:25:19 -0600

    Commit of lost edits..

--- src/Ten24/Silex/LiveReloadServiceProvider/LiveReloadServiceProvider.php
+++ src/Ten24/Silex/LiveReloadServiceProvider/LiveReloadServiceProvider.php
@@ -2,9 +2,6 @@
 /*
  * This file is part of LiveReloadServiceProvider. 
  * 
- * Thanks to Tobiass Josten for inspiration for his [unknown] mentorship
- * @url https://github.com/tobiassjosten/ResponsibleServiceProvider
- * 
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code. 
  * 
@@ -19,42 +16,37 @@ class LiveReloadServiceProvider implements ServiceProviderInterface
 {
     /**
      * Register this service
-     * 
-     * Set the default options & replace with applicaiton-defined options
-     * 
      * (non-PHPdoc)
      * @see \Silex\ServiceProviderInterface::register()
      */
     public function register(Application $app)
     {
         $defaults = array(
-                'host' => 'localhost',
+                'host' => 'here',
                 'port' => 35729,
                 'enabled' => true,
                 'check_server_presence' => true);
-        
-        if($app->offsetExists('ten24.livereload.options'))
+    
+        if ($app->offsetExists('ten24.livereload.options'))
         {
-            $app['ten24.livereload.options'] = $app->share(function() {
-                array_merge(
-                    $app['ten24.livereload.options'],
-                    $defaults);
-            });
+            $app['ten24.livereload.options'] = array_replace(
+                $defaults,
+                $app['ten24.livereload.options']);
+        }
+        else 
+        {
+            $app['ten24.livereload.options'] = $defaults;
         }
     }
 
     /**
-     * Bootstraps the application.
-     * 
-     * Adds the LiveReloadListener
-     * 
+     * Add the listener
      * (non-PHPdoc)
      * @see \Silex\ServiceProviderInterface::boot()
      */
     public function boot(Application $app)
     {
         $app['dispatcher']->addSubscriber(
-            new LiveReloadListener(), 
-            $app['ten24.livereload.options']);
+            new LiveReloadListener($app['ten24.livereload.options']));
     }
 }
